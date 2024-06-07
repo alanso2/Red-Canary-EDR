c820581616b29c003373bfabcc4a231bd3ed4fba
alanso2 <so.alan1996@gmail.com>
2024-06-07 16:30:27 -0400

    test main

--- main.rb
+++ main.rb
@@ -3,4 +3,6 @@ require_relative 'end_point_detection_response'
 # Start of script
 endpoint_detection = EndPointDetectionResponse.new
 application_path = "/Users/alanso/Desktop/Google Chrome.app"
-endpoint_detection.start_process(application_path, "--incognito 'https://www.yahoo.com'")
+# endpoint_detection.start_process(application_path, "--incognito 'https://www.yahoo.com'")
+create_path = "/Users/alanso/Desktop/Projects/Red-Canary-EDR/test_text_files"
+endpoint_detection.create_file(create_path, "txt")
